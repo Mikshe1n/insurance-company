5d1021c97df8be343b92f948240046f069887822
Mikshe1n <Mishaili4ev@yandex.ru>
2023-05-26 19:08:28 +0300

    6-slide

--- scripts.js
+++ scripts.js
@@ -1,3 +1,46 @@
-document.querySelector(".view").addEventListener("click", function(e) {
-    document.querySelector(".arrow").classList.toggle("active");
-    });
+// function viewDiv() {
+//     document.getElementById("block-vis").style.display = "block";
+//   }
+
+// function hideDiv() {
+//     document.getElementById("block-vis").style.display = "none";
+//   }
+
+// function viewDiv() {
+//     var block = document.getElementById("block-vis");
+//     block.style.display = "block";
+  
+//     var animBlock = document.getElementById(".anim-block");
+//     animBlock.addEventListener("click", function() {
+//       block.style.display = "none";
+//     });
+//   }
+
+var activeDiv = null; // Инициализируем переменную для активного div
+
+function showDiv(targetDiv) {
+  if (activeDiv) {
+    activeDiv.style.display = "none"; // Скрываем предыдущий активный div
+  }
+
+  targetDiv.style.display = "block"; // Отображаем целевой div
+  activeDiv = targetDiv; // Обновляем активный div
+}
+
+document.addEventListener("click", function(event) {
+  var target = event.target;
+
+  if (target.classList.contains("show-div")) {
+    var divId = target.getAttribute("data-div-id");
+    var targetDiv = document.getElementById(divId);
+
+    if (targetDiv) {
+      showDiv(targetDiv);
+    }
+  } else if (!target.classList.contains("target-div")) {
+    if (activeDiv) {
+      activeDiv.style.display = "none"; // Скрываем активный div при клике вне целевых div
+      activeDiv = null; // Сбрасываем активный div
+    }
+  }
+});
